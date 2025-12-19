20e620d876eb42c59517d5810afd7070d5b74e46
seejay <seejay181@gmail.com>
2025-12-19 17:51:12 -0600

    try another fix

--- server/FastAPI/fastAPI.py
+++ server/FastAPI/fastAPI.py
@@ -1,5 +1,6 @@
 # FastAPI/fastAPI.py
 from fastapi import FastAPI, HTTPException, Request
+from fastapi.middleware.cors import CORSMiddleware
 from pydantic import BaseModel
 import httpx
 import os
